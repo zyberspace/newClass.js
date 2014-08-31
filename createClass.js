a5e65ed80585c78ff9edf3ae463725f3588ab807
zyberspace <zyberspace@zyberware.org>
2014-08-31 05:47:14 +0200

    Basic class-handling with visibility and constructors

---
+++ createClass.js
@@ -0,0 +1,57 @@
+var createClass = function() {
+    var private = {
+        "classes": {}, //All defined classes will be saved here
+        "cloneObject": function(originalObject) { //Simple deep-object-cloning-function
+            var clonnedObject = {};
+            for (key in originalObject) {
+                if (typeof originalObject[key] !== "object") {
+                    clonnedObject[key] = originalObject[key];
+                } else {
+                    clonnedObject[key] = private.cloneObject(originalObject[key]);
+                }
+            }
+            return clonnedObject;
+        },
+        "createObject": function(classId, contructArguments) { //Creates a object from the class (defined by clasId)
+            var classOptions = private.classes[classId];
+            if (!classOptions) {
+                throw "Error: There is no class registered with the id \"" + classId + "\"!";
+            }
+
+            //Build object
+            var object = private.cloneObject(classOptions.public);
+            var privateObject = {
+                "private": private.cloneObject(classOptions.private),
+                "protected": private.cloneObject(classOptions.protected)
+            };
+            for (key in object) {
+                if (object[key].bind) {
+                    object[key] = object[key].bind(privateObject);
+                }
+                privateObject[key] = object[key];
+            }
+
+            //Call constructor
+            (object["__construct"] || object["__"] || function() {}).apply(privateObject, contructArguments);
+
+            return object;
+        }
+    };
+
+    return function(classOptions) {
+        do {
+            var classId = Math.random().toString(36).substr(2);
+        } while (typeof private.classes[classId] !== "undefined");
+        private.classes[classId] = classOptions;
+
+        var classCaller = function() {
+            return private.createObject(this.__id, arguments);
+        };
+        classCaller.prototype.__id = classId;
+        return classCaller;
+    };
+}();
+
+if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+    module.exports = createClass;
+}
